05f80c0b872eb117ca2a5a104961e9bf33b74589
Ainur Rohman <ainurrohman869@gmail.com>
2024-03-18 15:52:36 +0700

    update

---
+++ Deploy on Streamlit/Calories Burned Calculator/calories_burned_calculator_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+model = pickle.load(open('model.sav', 'rb'))
+
+st.write("Calories Burned Calculator App")
+
+gender = st.selectbox("Select Gender", ("Male","Female"))
+
+if (gender=="Male"):
+ g = 0
+else:
+ g = 1
+ 
+age = st.number_input("Enter Age", 1, 100, 1)
+height = st.number_input("Enter Height in cm", 1, 200, 1)
+weight = st.number_input("Enter Weight in kg", 1, 200, 1)
+duration = st.number_input("Enter Workout Duration in minutes", 1, 200, 1)
+heart_rate = st.number_input("Enter Average Heart Rate", 1, 200, 1) 
+body_temp = st.number_input("Enter Body Temperature in Celsius", 1, 100, 1) 
+prediction = model.predict(pd.DataFrame(columns=['Gender','Age','Height','Weight','Duration','Heart_Rate','Body_Temp'],
+                                         data = np.array([g,age,height,weight,duration,heart_rate,body_temp]).reshape(1,7)))
+ 
+if st.button("Predict"):
+ st.success(f"Calories Burned is {round(prediction[0],2)}")
