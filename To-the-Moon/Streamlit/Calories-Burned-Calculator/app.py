d81990b383c1dba72ba0dca28d6c2a6b929226f3
Ainur Rohman <ainurrohman869@gmail.com>
2024-03-19 10:09:45 +0700

    Update app.py

--- To-the-Moon/Streamlit/Calories-Burned-Calculator/app.py
+++ To-the-Moon/Streamlit/Calories-Burned-Calculator/app.py
@@ -5,11 +5,11 @@ import pandas as pd
 import numpy as np
 
 if "STREAMLIT_SHARE_URL" in os.environ:
- # model = pickle.load(open('model.sav', 'rb'))
- model = pickle.load(open('To-the-Moon/Streamlit/Calories-Burned-Calculator/model.sav', 'rb'))
-else:
- # model = pickle.load(open('To-the-Moon/Streamlit/Calories-Burned-Calculator/model.sav', 'rb'))
  model = pickle.load(open('model.sav', 'rb'))
+ # model = pickle.load(open('To-the-Moon/Streamlit/Calories-Burned-Calculator/model.sav', 'rb'))
+else:
+ model = pickle.load(open('To-the-Moon/Streamlit/Calories-Burned-Calculator/model.sav', 'rb'))
+ # model = pickle.load(open('model.sav', 'rb'))
 
 st.write("Calories Burned Calculator App")
 
