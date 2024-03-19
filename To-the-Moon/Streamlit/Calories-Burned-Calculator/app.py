93ae04fdbcc936d72f194c23ced2bc7d2d16cf82
Ainur Rohman <ainurrohman869@gmail.com>
2024-03-19 09:12:07 +0700

    Update app.py

--- To-the-Moon/Streamlit/Calories-Burned-Calculator/app.py
+++ To-the-Moon/Streamlit/Calories-Burned-Calculator/app.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 import numpy as np
 
-if 'IS_STREAMLIT_SHARE' in os.environ:
+if 'IS_STREAMLIT_SHARE' not in os.environ:
  model = pickle.load(open('To-the-Moon/Streamlit/Calories-Burned-Calculator/model.sav', 'rb'))
 else:
  model = pickle.load(open('model.sav', 'rb'))
