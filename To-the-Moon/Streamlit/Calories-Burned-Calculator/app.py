c41d6eb1c5af47580aee7b440b99c2832a610f62
Ainur Rohman <ainurrohman869@gmail.com>
2024-03-19 09:34:54 +0700

    Update app.py

--- To-the-Moon/Streamlit/Calories-Burned-Calculator/app.py
+++ To-the-Moon/Streamlit/Calories-Burned-Calculator/app.py
@@ -4,7 +4,7 @@ import sys
 import pandas as pd
 import numpy as np
 
-if "streamlit" not in sys.modules:
+if "streamlit" in sys.modules:
  model = pickle.load(open('model.sav', 'rb'))
 else:
  model = pickle.load(open('To-the-Moon/Streamlit/Calories-Burned-Calculator/model.sav', 'rb'))
